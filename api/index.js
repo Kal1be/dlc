ada31dcf7d510db842168366da9e909222401fc1
kal1be <yahoulbofelix@gmail.com>
2024-03-31 16:42:34 -0700

    add the resolve path to the project

--- api/index.js
+++ api/index.js
@@ -21,9 +21,9 @@ app.use("/api/auth", authRoutes)
 app.use("/api/post", postRoutes)
 app.use("/api/comment", commentRoutes)
 app.use(express.static(path.join(__dirname, "/client/dist")))
-console.log(path.join(__dirname, 'client', 'dist', 'index.html'));
+
 app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
 })
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
